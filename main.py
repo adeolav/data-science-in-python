0f53a834ecd469e7006959e7dc098f89ea7ccc07
adeolav <95139065+adeolav@users.noreply.github.com>
2022-12-22 14:04:14 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+"""Missing Puppy Report for Bayes"""
+import numpy as np
+import statsmodels as sm
+import seaborn as sns
+
+# Fill in Bayes' age (4.0)
+bayes_age = 4.0
+
+# Display the variable bayes_age
+print(bayes_age)
+
+# Bayes' favorite toy
+favorite_toy = "Mr. Squeaky"
+
+# Bayes' owner
+owner = "DataCamp"
+
+# Display variables
+print(favorite_toy)
+print(owner)
+
+birthday = "2017-07-14"
+case_id = "DATACAMP!123-456?"
